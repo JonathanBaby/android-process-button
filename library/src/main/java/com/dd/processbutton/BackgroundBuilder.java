4cd0a622ea9cf5401ccd9e9081c08c51b53f9ac3
Jonathan BABY <baby.jonathan@gmail.com>
2015-10-31 17:22:02 +0100

    Merge XML attributes with default tint ColorStateList

--- library/src/main/java/com/dd/processbutton/BackgroundBuilder.java
+++ library/src/main/java/com/dd/processbutton/BackgroundBuilder.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.RippleDrawable;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.util.AttributeSet;
 
@@ -40,24 +41,45 @@ public class BackgroundBuilder {
     }
 
     private Drawable setupDrawable(TypedArray attr, int cornerRadius) {
-        ColorStateList tintStateList = context.getResources().getColorStateList(R.color.btn_tint_selector);
+        ColorStateList colorStateList = getMergedColorStateList(attr);
         GradientDrawable roundedRectDrawable = (GradientDrawable) getDrawable(R.drawable.rounded_rect).mutate();
 
         roundedRectDrawable.setCornerRadius(cornerRadius);
         Drawable drawable = DrawableCompat.wrap(roundedRectDrawable);
-        DrawableCompat.setTintList(drawable, tintStateList);
+        DrawableCompat.setTintList(drawable, colorStateList);
         return drawable;
     }
 
+
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private Drawable setupDrawableV21(TypedArray attr, int cornerRadius) {
-        ColorStateList tintStateList = context.getResources().getColorStateList(R.color.btn_tint_selector);
+        ColorStateList colorStateList = getMergedColorStateList(attr);
         RippleDrawable rippleDrawable = (RippleDrawable) getDrawable(R.drawable.rounded_rect);
         GradientDrawable roundedRectDrawable = (GradientDrawable) rippleDrawable.getDrawable(0);
 
         roundedRectDrawable.setCornerRadius(cornerRadius);
-        rippleDrawable.setTintList(tintStateList);
+        roundedRectDrawable.setTintList(colorStateList);
         return rippleDrawable;
+    }
+
+    @NonNull
+    private ColorStateList getMergedColorStateList(TypedArray attr) {
+        return new ColorStateList(
+                new int[][]
+                        {
+                                new int[]{-android.R.attr.state_enabled},
+                                new int[]{android.R.attr.state_pressed},
+                                new int[]{android.R.attr.state_focused},
+                                new int[]{}
+                        },
+                new int[]
+                        {
+                                attr.getColor(R.styleable.FlatButton_pb_colorDisabled, getColor(R.color.btn_disabled)),
+                                attr.getColor(R.styleable.FlatButton_pb_colorPressed, getColor(R.color.btn_pressed)),
+                                attr.getColor(R.styleable.FlatButton_pb_colorFocused, getColor(R.color.btn_focused)),
+                                attr.getColor(R.styleable.FlatButton_pb_colorNormal, getColor(R.color.btn_normal))
+                        }
+        );
     }
 
     public float getDimension(int id) {
